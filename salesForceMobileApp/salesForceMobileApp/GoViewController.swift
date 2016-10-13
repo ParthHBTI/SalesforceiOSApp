4b4ea4f5988f30f913b4403646ff4a3cdd600e0e
ParthHBTI <kkumar@kloudrac.com>
2016-10-13 23:06:42 +0530

    Contact And Opportunity added in SlideMenuVC

--- salesForceMobileApp/salesForceMobileApp/GoViewController.swift
+++ salesForceMobileApp/salesForceMobileApp/GoViewController.swift
@@ -7,15 +7,93 @@
 //
 
 import UIKit
+import SalesforceRestAPI
 
-class GoViewController: UIViewController {
+class GoViewController: UIViewController, SFRestDelegate{
+    
+@IBOutlet weak var tableView: UITableView!
+    var dataRows = [NSDictionary]()
+    var resArr:AnyObject = []
+    // MARK: - View lifecycle
+    override func loadView()
+    {
+        super.loadView()
+        self.title = "Mobile SDK Sample App"
+        
+        //Here we use a query that should work on either Force.com or Database.com
+        let request = SFRestAPI.sharedInstance().requestForQuery("SELECT Name FROM Opportunity");
+        SFRestAPI.sharedInstance().send(request, delegate: self);
+        
+    }
+    
+    // MARK: - SFRestAPIDelegate
+    func request(request: SFRestRequest, didLoadResponse jsonResponse: AnyObject)
+    {
+        self.dataRows = jsonResponse["records"] as! [NSDictionary]
+        self.log(.Debug, msg: "request:didLoadResponse: #records: \(self.dataRows.count)")
+        dispatch_async(dispatch_get_main_queue(), {
+            self.resArr = self.dataRows
+            self.tableView.reloadData()
+        })
+    }
+    
+    func request(request: SFRestRequest, didFailLoadWithError error: NSError)
+    {
+        self.log(.Debug, msg: "didFailLoadWithError: \(error)")
+        // Add your failed error handling here
+    }
+    
+    func requestDidCancelLoad(request: SFRestRequest)
+    {
+        self.log(.Debug, msg: "requestDidCancelLoad: \(request)")
+        // Add your failed error handling here
+    }
+    
+    func requestDidTimeout(request: SFRestRequest)
+    {
+        self.log(.Debug, msg: "requestDidTimeout: \(request)")
+        // Add your failed error handling here
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerCellNib(DataTableViewCell.self)
     }
-
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.setNavigationBarItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension GoViewController : UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return DataTableViewCell.height()
+    }
+}
+
+extension GoViewController : UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataRows.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier(DataTableViewCell.identifier) as! DataTableViewCell
+        //        let data = DataTableViewCellData(imageUrl: "dummy", text: dataRows.objectAtIndexPath(indexPath.row)[""])
+        //        cell.setData(data)
+        cell.textLabel?.text = resArr.objectAtIndex(indexPath.row)["Name"] as? String
+        print(cell.textLabel?.text)
+        return cell
     }
 }

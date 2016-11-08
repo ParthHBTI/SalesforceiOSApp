ca24b933da1e35fc5f25c771e057d83d0b1faff4
ParthHBTI <kkumar@kloudrac.com>
2016-11-08 17:56:46 +0530

    call chatter and RestAPI for sharing Data

--- salesForceMobileApp/salesForceMobileApp/AttachViewController.swift
+++ salesForceMobileApp/salesForceMobileApp/AttachViewController.swift
@@ -28,12 +28,59 @@ let imagePicker = UIImagePickerController()
     func shareAction() {
         var image = UIImage()
         image = imageView.image!
-        let imageData = UIImageJPEGRepresentation(image, 0.0)
-        let request = SFRestAPI.sharedInstance().requestForUploadFile(imageData!, name: "plus.png", description: "Test Img", mimeType: "image/png")
-        SFRestAPI.sharedInstance().send(request, delegate: self)
-        print(request)
+        //let imageData = UIImageJPEGRepresentation(image, 1.0)
+        
+//        let request = SFRestAPI.sharedInstance().requestForUploadFile(imageData!, name: "plus.png", description: "Test Img", mimeType: "image/png")
+//        SFRestAPI.sharedInstance().send(request, delegate: self)
+//        print(request)
+
+//       let entID = SFUserAccountManager.sharedInstance().currentUser?.idData.orgId
+//        let req = SFRestAPI.sharedInstance().requestForAddFileShare(entID!, entityId: entID!, shareType: "V")
+//         SFRestAPI.sharedInstance().send(req, delegate: self)
     }
     
+     func request(request: SFRestRequest, didLoadResponse dataResponse: AnyObject) {
+        if request.method == SFRestMethod.POST {
+            shareFile(dataResponse.objectForKey("id") as! String)
+        } else {
+            print("")
+        }
+        let attachmentId = (dataResponse["id"] as! String)
+        let range = (request.path as NSString).rangeOfString("/feed-items")
+         createFeedForAttachmentId(attachmentId)
+    }
+    
+    func shareFile(field: String) {
+    let entID = SFUserAccountManager.sharedInstance().currentUser?.idData.orgId
+        let req = SFRestAPI.sharedInstance().requestForAddFileShare(field, entityId: entID!, shareType: "V")
+        SFRestAPI.sharedInstance().send(req, delegate: self)
+    }
+    
+    
+    func createFeedForAttachmentId(attachmentId: String) {
+//        let filePath = NSBundle.mainBundle().pathForResource("feedTemplate", ofType: "json")!
+//        let url = NSURL.fileURLWithPath(filePath)
+//        let error: NSError? = nil
+//        let feedJSONTemplateData = try! NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+//        var feedJSONTemplateStr = String(data: feedJSONTemplateData, encoding: NSUTF8StringEncoding)
+//        feedJSONTemplateStr = feedJSONTemplateStr!.stringByReplacingOccurrencesOfString("__BODY_TEXT__", withString: "kloudrac")
+//        feedJSONTemplateStr = feedJSONTemplateStr!.stringByReplacingOccurrencesOfString("__ATTACHMENT_ID__", withString: attachmentId)
+//        let data = feedJSONTemplateStr!.dataUsingEncoding(NSUTF8StringEncoding)
+//        if let jsonObj: AnyObject = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject] {
+//            if let dict = jsonObj as? NSDictionary {
+//                print(dict)
+//            } else {
+//                print("not a dictionary")
+//            }
+//        } else {
+//            print("Could not parse JSON: \(error!)")
+//        }
+//        let api = SFRestAPI.sharedInstance()
+//        _ = "/\(api.apiVersion)/chatter/feeds/record//feed-items/"
+//        
+//        
+ 
+    }
     @IBAction func attachPopOver(sender: AnyObject) {
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .PhotoLibrary
@@ -42,8 +89,17 @@ let imagePicker = UIImagePickerController()
 
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
             imageView.image = pickedImage
+            let imageData: NSData = UIImageJPEGRepresentation(pickedImage, 0.0)!
+            let req = SFRestAPI.sharedInstance().requestForUploadFile(imageData, name: "TestImage.png", description: "Share Image", mimeType: "image/png")
+            SFRestAPI.sharedInstance().send(req, delegate: self)
         }
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     }
